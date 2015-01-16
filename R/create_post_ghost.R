29f4e3a392ec794ea0522b662cb29b0c6e839ff6
Bob Rietveld <Bob@Bobs-MacBook-Pro.local>
2015-01-16 15:38:26 +0100

    Changed response handler

--- R/create_post_ghost.R
+++ R/create_post_ghost.R
@@ -5,5 +5,5 @@ create_post_ghost <- function(post_body){
   #Make the Call
   post_ghost <- make_api_call("POST",ghost_url,post_body)
 
-  return(parse_content(post_ghost))
+  return(construct_response(post_ghost))
 }
